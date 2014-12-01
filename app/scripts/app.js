c6f93f867a4d1599471ab75896a24cc803dd0af4
Yasa Akbulut <yasakbulut@gmail.com>
2014-12-01 23:03:54 +0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name posytifApp
+ * @description
+ * # posytifApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('posytifApp', [
+    'ngTouch'
+  ]);
