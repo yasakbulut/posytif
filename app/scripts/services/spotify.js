fdc41ec38f271c3339f87982a52e81786427a7a0
Yasa Akbulut <yasakbulut@gmail.com>
2014-12-04 18:20:39 +0200

    start of the day commit
    add caching to spotify requests
    fix font-awesome dist problem

--- app/scripts/services/spotify.js
+++ app/scripts/services/spotify.js
@@ -13,37 +13,40 @@
 angular.module('posytifApp')
   .factory('SpotifyService', function ($http) {
 
+    var defaultConfig = {
+      cache: true
+    };
     var search = function(query, type){
       //Search for all items by default
       type = type||'album,artist,track';
-      return $http.get('https://api.spotify.com/v1/search?q='+query+'&type='+type+'&limit=50').
+      return $http.get('https://api.spotify.com/v1/search?q='+query+'&type='+type+'&limit=50', defaultConfig).
         then(function(response){
           return response.data;
         });
     };
 
     var getTopTracks = function(artistId, country){
-      return $http.get('https://api.spotify.com/v1/artists/'+artistId+'/top-tracks?country='+country+'').
+      return $http.get('https://api.spotify.com/v1/artists/'+artistId+'/top-tracks?country='+country, defaultConfig).
         then(function(response){
           return response.data.tracks;
         });
     };
 
     var getArtist = function(artistId){
-      return $http.get('https://api.spotify.com/v1/artists/'+artistId).
+      return $http.get('https://api.spotify.com/v1/artists/'+artistId, defaultConfig).
         then(function(response){
           return response.data;
         });
     };
 
     var getAlbumsOfArtist = function(artistId, country){
-      return $http.get('https://api.spotify.com/v1/artists/'+artistId+'/albums?country='+country).
+      return $http.get('https://api.spotify.com/v1/artists/'+artistId+'/albums?country='+country, defaultConfig).
         then(function(response){
           return response.data.items;
         });
     };
     var getTracksOfAlbum = function(albumId){
-      return $http.get('https://api.spotify.com/v1/albums/'+albumId+'/tracks').
+      return $http.get('https://api.spotify.com/v1/albums/'+albumId+'/tracks', defaultConfig).
         then(function(response){
           return response.data.items;
         });
